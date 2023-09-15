b8748a79dc99147aa271594dea187d1464b4c9b5
Chris James <quii@hey.com>
2023-09-15 10:00:11 +0100

    more tidying imports that went silly

--- adapters/driven/persistence/sqlite/recipe_store_test.go
+++ adapters/driven/persistence/sqlite/recipe_store_test.go
@@ -1,7 +1,7 @@
 package sqlite_test
 
 import (
-	sqlite2 "github.com/quii/go-fakes-and-contracts/adapters/driven/persistence/sqlite"
+	"github.com/quii/go-fakes-and-contracts/adapters/driven/persistence/sqlite"
 	"github.com/quii/go-fakes-and-contracts/domain/planner"
 	"testing"
 )
@@ -9,7 +9,7 @@ import (
 func TestRecipeStore(t *testing.T) {
 	planner.RecipeBookContract{
 		NewBook: func() planner.RecipeBook {
-			return sqlite2.NewRecipeStore(sqlite2.NewSQLiteClient())
+			return sqlite.NewRecipeStore(sqlite.NewSQLiteClient())
 		},
 	}.Test(t)
 }
